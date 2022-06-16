32287ea17dffe9de9bb423a4c959b764fc347851
emel95 <amalmosavi95@gmail.com>
2022-06-16 20:03:15 +0430

    test chackBox

--- src/components/Checkbox/__test__/CheckBox.test.js
+++ src/components/Checkbox/__test__/CheckBox.test.js
@@ -1,22 +1,50 @@
-// import React from 'react';
-// import CheckBox from '../CheckBox';
-// import Icon from 'react-native-vector-icons/MaterialIcons';
-// import { fireEvent, render, wait } from '@testing-library/react-native';
+import React from 'react';
+import CheckBox from '../CheckBox';
+import { fireEvent, render, wait } from '@testing-library/react-native';
 
 
-// describe('CheckBox', () => {
-//     jest.mock('react-native-vector-icons/MaterialIcons', () => 'Icon')
-//   const activeCheckBox  = render(<CheckBox title='music' />);
+describe('CheckBox', () => {
+    jest.mock(
+        'react-native-vector-icons/MaterialIcons',
+        () => 'MaterialIcons',
+    );
+    
+    const activeCheckBox = render(<CheckBox title='music'  />);
+    
+    it('renders correctly', () => {
+        const activeCheckBoxAsJson = activeCheckBox.toJSON();
+        expect(activeCheckBoxAsJson).toMatchSnapshot();
+    });
+    // it('performs onValueChange', () => {
+    //     const { getByTestId } = render(<CheckBox title='music' />);
+    //     const checkBox = getByTestId('custtomCheckBox');
+    //     fireEvent(checkBox,'handelPress');
+    //     expect(checkBox.handelPress).toBeCalled();
+    // });
 
-//    const { getByTestId, }= activeButton ;
-//   it('renders correctly', () => {
-//     const activeCheckBoxAsJson = activeCheckBox.toJSON();
-//     expect(activeCheckBoxAsJson).toMatchSnapshot();
 
-//     // expect(getByText(caption)).toBeTruthy();
-//     // fireEvent.press(getByTestId("custtomButton"));
+    it('shows the tickIcon when the checkBox checked', () => {
+        const checked = false;
+        const { getByTestId } = render(
+            <CheckBox isChecked={checked} title='music' />
+        );
+        const checkBoxById = getByTestId('custtomCheckBox');
+        expect(checkBoxById).toBeDefined();
+        fireEvent.press(checkBoxById);
+        expect(checkBoxById).toBeTruthy();
+       
 
-//   });
-// });
+    });
+    // it('changes value Is true', () => {
+    //     let checked = true;
+    //     const { getByTestId } = render(
+    //         <CheckBox isChecked={checked} title='music' />
+    //     );
+    //     const checkBox = getByTestId('custtomCheckBox');
+    //     expect(checked).toBeTruthy();
+    //     fireEvent.press(checkBox, false);
+
+    // });
+});
 
 
