be38b0558820b46b1daab391960d8c7002c69b4f
dariush <dariushtaghavi9797@gmail.com>
2022-06-17 12:51:21 +0430

    feat: test text input

--- App/index.js
+++ App/index.js
@@ -1,1 +1,34 @@
-export default from '../storybook';
+// export default from '../storybook';
+import {Input, RadioButton} from '../src/components';
+import React, {useState} from 'react';
+import {View} from 'react-native';
+import styles from '../src/components/RadioButton/style';
+
+const App = () => {
+  const [user, setUser] = useState('');
+  const data = [
+    {value: '1', title: 'data 1'},
+    {value: '2', title: 'data 2'},
+    {value: '3', title: 'data 3'},
+  ];
+  return (
+    <View>
+      <Input
+        testID="text-input"
+        value={user}
+        onChangeText={setUser}
+        placeholder={'type here'}
+      />
+      <RadioButton
+        testID="radio-button"
+        data={data}
+        onPress={value => {
+          console.log(data[value - 1]);
+        }}
+        selectedStyle={styles.bigSelected}
+        unselectedStyle={styles.bigUnselected}
+      />
+    </View>
+  );
+};
+export default App;
--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {View, TextInput} from 'react-native';
 import styles from './style';
 
-const Input = ({onChangeText, value, placeholder, style}) => {
+const Input = ({onChangeText, value, placeholder, style, testID}) => {
   return (
     <View style={[styles.main, {...style}]}>
       <TextInput
         placeholder={placeholder}
         onChangeText={onChangeText}
         value={value}
+        testID={testID}
       />
     </View>
   );
