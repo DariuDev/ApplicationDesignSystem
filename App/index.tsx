3e848f6b51ae879739e19c0342f94a3daa7aef3b
dariush <dariushtaghavi9797@gmail.com>
2022-06-21 10:32:17 +0430

    refactor: change js to ts

---
+++ App/index.tsx
@@ -0,0 +1,33 @@
+//  export default from '../storybook';
+import {Input, RadioButton} from '../src/components';
+import React, {useState} from 'react';
+import {View} from 'react-native';
+import styles from '../src/components/RadioButton/style';
+
+const App = () => {
+  const [user, setUser] = useState('');
+  const data = [
+    {value: '1', title: 'data 1'},
+    {value: '2', title: 'data 2'},
+  ];
+  return (
+    <View>
+      <Input
+        testID="text-input"
+        value={user}
+        onChangeText={setUser}
+        placeholder={'type here'}
+      />
+      <RadioButton
+        testID="radio-button"
+        data={data}
+        onPress={(index) => {
+           console.log(data[index - 1].title);
+        }}
+        selectedStyle={styles.smallSelected}
+        unselectedStyle={styles.bigUnselected}
+      />
+    </View>
+  );
+};
+export default App;
