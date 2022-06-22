966cbef82edeab97469321d796b6a9b33e4ad6ba
dariush <dariushtaghavi9797@gmail.com>
2022-06-22 12:58:31 +0430

    test: test textInput

--- src/components/TextInput/__test__/TextInput.test.js
+++ src/components/TextInput/__test__/TextInput.test.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {render, fireEvent} from '@testing-library/react-native';
 import TextInput from '..';
 
+const Example = () => { 
+  const [value, setValue] = useState('');
+  return(
+    <TextInput placeholder={'type here'} testID={'test'} value={value} onChangeText={setValue} />
+)}
+
 describe('components =>  TextInput', () => {
   it('test textInput', async () => {
-    const screen = render(
-      <TextInput
-        placeholder={'type here'}
-        testID={'test'}
-      />,
-    );
-    expect(screen.getByTestId('test')).toHaveProp('placeholder', 'type here');
+    expect.assertions(4);
+    const screen = render(<Example />);
+    const input = screen.getByTestId('test');
+    expect(input).toHaveProp('placeholder', 'type here');
+    expect(input).toHaveProp('testID', 'test');
+
+    fireEvent.changeText(input, 'dariush');
+    expect(input.props.value).toBe('dariush');
+
+    fireEvent.changeText(input, 'soqra');
+    expect(input.props.value).toBe('soqra');
   });
-});
+  });
