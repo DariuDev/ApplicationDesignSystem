c9d22237268e4885b8e46420e9cbebcd8ca289b0
dariush <dariushtaghavi9797@gmail.com>
2022-06-26 10:35:30 +0430

    fix: fix color constants

---
+++ src/constants/colors/Colors.ts
@@ -0,0 +1,20 @@
+export const COLORS = {
+  DARK: '#727586',
+  LIGHT: '#d1d4e3',
+ RED : {
+  PRIMARY: '#db4447',
+  SECONDARY: '#cb1d20',
+  DARK: '#910b0d',
+},
+ GREEN : {
+  PRIMARY: '#3fdf4e',
+  SECONDARY: '#11c121',
+  DARK: '#088b14',
+},
+ BLUE : {
+  PRIMARY: '#247fff',
+  SECONDARY: '#2460ff',
+  DARK: '#2444ff',
+},
+};
+
