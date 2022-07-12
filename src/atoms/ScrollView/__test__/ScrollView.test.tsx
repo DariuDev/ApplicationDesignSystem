a31a5346e4f4e1ba3cc9e72d7b0ea9f57d5be7a0
dariush <dariushtaghavi9797@gmail.com>
2022-07-12 15:17:12 +0430

    test : test scrollView

---
+++ src/atoms/ScrollView/__test__/ScrollView.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {render, fireEvent} from '@testing-library/react-native';
+import ScrollView from '../index';
+
+describe('atoms => ScrollView', () => {
+  const onScrollMock = jest.fn();
+  const eventData = {
+    nativeEvent: {
+      contentOffset: {
+        y: 200,
+      },
+    },
+  };
+  it('test scrollView', () => {
+    const screen = render(
+     <ScrollView testID={'scrollview-id'} onScroll={onScrollMock}>
+        <Text>Dariush</Text>
+      </ScrollView>
+    );
+    const scrollView = screen.getByTestId('scrollview-id')
+    
+    fireEvent.scroll(scrollView, eventData);
+  });
+});
