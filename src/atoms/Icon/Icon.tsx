1c0c7facbd3a7da6266c5bd8e6ccef04c53fa408
emel95 <amalmosavi95@gmail.com>
2022-07-13 10:19:16 +0430

    test:icon

--- src/atoms/Icon/Icon.tsx
+++ src/atoms/Icon/Icon.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { IIcon } from './type';
 
 
-const Icon = ({ testID,name, type, size, color, style, onPress }: IIcon) => {
+const Icon = ({ testID, name, type, size, color, style, onPress }: IIcon) => {
     const Tag = type // custom element type
-    return (
-        <>
-            {type && name && (
-                <Tag testID='icon-test' name={name} size={size} color={color} style={{...style}} onPress={onPress} />
-            )}
-        </>
-    )
+    if (type && name) {
+        return (<Tag testID='icon-test' name={name} size={size} color={color} style={{ ...style }} onPress={onPress} />)
+    }
+    else {
+        return (null)
+    }
 }
 export default Icon;
 {/* <Icon type={props.item.type} name={props.item.icon} color={focused ? Colors.white : Colors.primary} /> */ }
