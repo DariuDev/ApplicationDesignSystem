787c96e24174e3c020ee93817f8308ef3d85dc2f
emel95 <amalmosavi95@gmail.com>
2022-07-23 12:36:27 +0430

    slider with timeOut

--- src/components/HeaderHomePageSlider/HeaderHomePageSlider.tsx
+++ src/components/HeaderHomePageSlider/HeaderHomePageSlider.tsx
@@ -1,20 +1,37 @@
-
-import React from 'react';
-import { View, Text, Image } from 'react-native';
-import {
-    ScrollView, Card, Button, Icon
-} from '../../atoms';
+import React, { useRef, useState, useEffect } from 'react';
+import { View, Text, Image, ScrollView } from 'react-native';
+import { Card, Button, Icon } from '../../atoms';
 import { MaterialIcons } from '../../atoms/Icon/type';
 import { styles } from './style';
 import { IHeaderHomePageSlider } from './HeaderHomePageSlider.type';
 
-
-
+const delay = 5;
 const HeaderHomePageSlider = ({ data, onPressTitle }: IHeaderHomePageSlider) => {
+    const lenght = data.length;
+    const [current, setCurrent] = useState<number>(0);
+    const sliderRef = useRef<any>(null);
+    useEffect(() => {
+        let timer = setTimeout(() => {
+            if (current < lenght - 2) {
+                setCurrent(prev => prev + 1);
+            } else {
+                setCurrent(0);
+            }
 
+            sliderRef?.current?.scrollToIndex({ index: current, animated: true });
+
+        }, delay * 1000);
+
+
+        return () => {
+            clearTimeout(timer);
+        };
+
+    });
     return (
         <ScrollView
             horizontal={true}
+            ref={sliderRef}
             showsHorizontalScrollIndicator={false}
             style={{ marginEnd: 10, marginTop: 10 }}>
             {data.map((item: any) => {
