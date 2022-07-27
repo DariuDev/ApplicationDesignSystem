40f04b9e7a758c89e3f16dc212ce3c91bbd7b951
emel95 <amalmosavi95@gmail.com>
2022-07-27 08:48:59 +0430

    test

--- src/components/organisms/TopBrands/TopBrandCard/TopBrandCard.tsx
+++ src/components/organisms/TopBrands/TopBrandCard/TopBrandCard.tsx
@@ -3,12 +3,12 @@ import { styles } from './style';
 import type{ ITopBrandCard } from './TopBrandCard.type';
 import { Card, Text, ScrollView } from '../../../atoms';
 
-const TopBrandCard = ({ data, onPressItem }: ITopBrandCard) => {
+const TopBrandCard = ({ data, onPressItem,testID='cardBrand-id' }: ITopBrandCard) => {
   return (
     <ScrollView showsHorizontalScrollIndicator={false} horizontal>
       {data.map((item: any) => {
         return (
-          <Card style={styles.card} onPress={onPressItem}>
+          <Card testID={testID} style={styles.card} onPress={onPressItem}>
             <Text>{item.txt}</Text>
           </Card>
         );
