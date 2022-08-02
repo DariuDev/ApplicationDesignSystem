786b226b3f3b6b71541f7f27d3179d8b0aaa5087
emel95 <amalmosavi95@gmail.com>
2022-08-02 15:35:46 +0430

    edit card brand

--- src/components/molecules/BrandCard/style.ts
+++ src/components/molecules/BrandCard/style.ts
@@ -1,15 +1,15 @@
-import { StyleSheet, Dimensions} from 'react-native';
-const SCREEN_WIDTH = Dimensions.get('window').width;
+import { StyleSheet, Dimensions } from 'react-native';
+// const SCREEN_WIDTH = Dimensions.get('window').width;
 const styles = StyleSheet.create({
   card: {
     marginHorizontal: 10,
     marginBottom: 20,
     borderWidth: 1,
     borderColor: '#e2e6f0',
-    flex: 1,
+    // flex: 1,
     borderRadius: 15,
-    width: SCREEN_WIDTH / 5,
-    height: SCREEN_WIDTH / 5,
+    width: 100,
+    height: 100,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: 'white',
